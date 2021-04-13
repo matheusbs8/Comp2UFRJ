5d04dc27543e5139bb78d2d54e3173b6e2fcd3be
Matheus Barroso <santanambarroso@gmail.com>
2021-04-13 14:35:16 -0300

    First commit

---
+++ LAB2/src/AritmeticaUtils.java
@@ -0,0 +1,20 @@
+//Nome : Matheus Barroso de Santana
+//DRE: 120041661
+
+public class AritmeticaUtils {
+
+    public static int mdc(int x, int y) {
+
+        if (x < 1 || y < 1) {
+            throw new ArithmeticException("Os parâmetros precisam ser positivos!");
+        }
+
+        int resto = x % y;
+
+        if (resto == 0) {
+            return y;
+        }
+
+        return mdc(y, resto);
+    }
+}
